26026b3786ee5ea5282cd3c33ee70fe4083f4db5
arunkabish1 <720721110060@hicet.ac.in>
2023-12-21 20:09:58 +0530

    second

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -11,15 +11,15 @@ let App: React.FunctionComponent<myProps> = () => {
       <div className="flex gap-4">
         <div className="border-2 p-4 border-red-500 rounded-lg ">
           <h1 className="text-center text-xl font-bold text-red-500 mb-5">Pending</h1>
-          <TaskCard title="Build the website" duedate="2022-01-01" assignee="Arun"/>
-          <TaskCard title="Add blog" duedate="2022-01-01" assignee="Kabish" />
+          <TaskCard title="Build the website" dueDate="2022-01-01" assigneeName="Arun"/>
+          <TaskCard title="Add blog" dueDate="2022-01-01" assigneeName="Kabish" />
           <h2 className="font-bold text-gray-500 p-2 "> + New Task</h2>
         </div>
         <div className="border-2 p-4 border-green-500 rounded-lg">
           <h1 className="text-center text-xl font-bold text-green-500 mb-5">Done</h1>
           <div>
-            <TaskCard title="Design mockup" assignee="Arun" completingDate="2022-01-01" />
-            <TaskCard title="Approval from Principal" assignee="Kabish" completingDate="2022-01-01"/>
+            <TaskCard title="Design mockup" assigneeName="Arun" completedAtDate="2022-01-01" />
+            <TaskCard title="Approval from Principal" assigneeName="Kabish" completedAtDate="2022-01-01"/>
           </div>
         </div>
       </div>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -3,24 +3,24 @@ import './TaskCard.css'
 
 interface TaskProps {
   title: string;
-  duedate?: string; 
-  completingDate?: string;
-  assignee: string;
+  dueDate?: string; 
+  completedAtDate?: string;
+  assigneeName: string;
 }
 const TaskCard: React.FunctionComponent<TaskProps> = (props) => {
   console.log(props);
   return (
     <div className='TaskItem'>
       <h2 className="text-l font-bold mb-1">{props.title}</h2>
-      {props.duedate && (
-        <p className='text-gray-500'>Duedate: {props.duedate}</p>
+      {props.dueDate && (
+        <p className='text-gray-500'>dueDate: {props.dueDate}</p>
       )}
 
-      {props.completingDate && (
-        <p className='text-gray-500'>Completed At: {props.completingDate}</p>
+      {props.completedAtDate && (
+        <p className='text-gray-500'>Completed At: {props.completedAtDate}</p>
       )}
 
-      <p className='text-gray-500'>Assignee: {props.assignee}</p>
+      <p className='text-gray-500'>assigneeName: {props.assigneeName}</p>
     </div>
   );
 }
