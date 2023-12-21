b7fa95d9b43f931331114deb31eae2de18c3976f
arunkabish1 <720721110060@hicet.ac.in>
2023-12-21 20:20:27 +0530

    second

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -13,14 +13,14 @@ const TaskCard: React.FunctionComponent<TaskProps> = (props) => {
     <div className='TaskItem'>
       <h2 className="text-l font-bold mb-1">{props.title}</h2>
       {props.dueDate && (
-        <p className='text-gray-500'>dueDate: {props.dueDate}</p>
+        <p className='text-gray-500'>Due on: {props.dueDate}</p>
       )}
 
       {props.completedAtDate && (
-        <p className='text-gray-500'>Completed At: {props.completedAtDate}</p>
+        <p className='text-gray-500'>Completed on: {props.completedAtDate}</p>
       )}
 
-      <p className='text-gray-500'>assigneeName: {props.assigneeName}</p>
+      <p className='text-gray-500'>Assignee: {props.assigneeName}</p>
     </div>
   );
 }
