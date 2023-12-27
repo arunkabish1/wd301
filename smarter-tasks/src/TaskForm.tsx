05b3ddd04daf4b2c7ef251609094c91e79018481
arunkabish1 <720721110060@hicet.ac.in>
2023-12-27 15:13:32 +0530

    l4 2

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -14,26 +14,45 @@ const TaskForm: React.FC<TaskFormProps> = ({ addTask }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (task.title.trim() !== "") {
+    if (task.title.trim() !== "" && task.dueDate?.trim() !== "") {
       addTask(task);
       setTask({ title: "", dueDate: "", description: "" });
     }
   };
 
+  const { title, dueDate, description } = task;
+
   return (
     <form onSubmit={handleSubmit} className="mt-4">
-      <div className="flex">
-        <input
-          type="text"
-          value={task.title}
-          onChange={(e) => setTask({ ...task, title: e.target.value })}
-          className="flex-1 border border-blue-300 rounded-md p-2"
-          placeholder="Any new task..."
-        />  
+      <div className="flex flex-col">
+        <div className="flex">
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTask({ ...task, title: e.target.value })}
+            className="flex-1 border border-blue-300 rounded-md p-2 required"
+            placeholder="Task Title"
+          />
+          <input
+            type="date"
+            value={dueDate}
+            onChange={(e) => setTask({ ...task, dueDate: e.target.value })}
+            className="ml-2 flex-1 border border-blue-300 rounded-md p-2 required"
+            placeholder="Due Date"
+          />
+        </div>
+        <div className="mt-2">
+          <textarea
+            value={description}
+            onChange={(e) => setTask({ ...task, description: e.target.value })}
+            className="w-full border border-blue-300 rounded-md p-2 required"
+            placeholder="Task Description"
+          />
+        </div>
         <button
           type="submit"
           id="addTaskButton"
-          className="ml-2 bg-green-500 hover:bg-green-600 hover:text-500 text-white rounded-md p-2"
+          className="mt-2 bg-green-500 hover:bg-green-600 hover:text-white text-white rounded-md p-2"
         >
           Add Task
         </button>
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -11,7 +11,8 @@ const TaskList: React.FC<TaskListProps> = ({ tasks, onDelete }) => {
     <ul className="mt-4">
       {tasks.map((task, index) => (
         <li key={index} className="flex justify-between items-center border-b border-blue-300 py-2">
-          <span className="font-bold px-2">{task.title}</span>
+          <span className="font-bold px-2">Title:{task.title}</span>
+          <span className="px-2 font-bold"> Due Date:{task.dueDate}</span>
           <button onClick={() => onDelete(index)} className="bg-red-500 rounded text-white hover:bg-red-300 hover:text-black border border-black-500 px-2 deleteTaskButton">
             Delete Task
           </button>
