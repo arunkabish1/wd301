066f6120b86c32dc579615c98bd4b5e07b1ad71b
arunkabish1 <720721110060@hicet.ac.in>
2023-12-28 11:48:15 +0530

    l5 1st

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,10 +1,21 @@
-import "./App.css";
-import TaskApp from "./TaskApp";
-function App() {
-  return (
-    <div className="App">
-      <TaskApp />
-    </div>
-  );
-}
+import React from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/tasks",
+    element: <TaskListPage />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
 export default App;
