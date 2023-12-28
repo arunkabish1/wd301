edf36a834be153485896a70fa78962173af2fb7a
arunkabish1 <720721110060@hicet.ac.in>
2023-12-28 11:50:43 +0530

    l5 1st resolve removing react,route

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TaskListPage from "./pages/TaskListPage";
 
