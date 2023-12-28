408a38907a01e73367f3e4189ba9a410205130ae
arunkabish1 <720721110060@hicet.ac.in>
2023-12-28 11:09:52 +0530

    l4 resolve l1

--- smarter-tasks/src/TaskApp.tsx
+++ smarter-tasks/src/TaskApp.tsx
@@ -1,14 +1,14 @@
+import React from "react";
 import { TaskItem } from "./types";
 import TaskForm from "./TaskForm";
 import TaskList from "./TaskList";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 
-interface TaskAppProp {}
 interface TaskAppState {
   tasks: TaskItem[];
 }
 
-const TaskApp = (props: TaskAppProp) => {
+const TaskApp: React.FC = () => {
   const [taskAppState, setTaskAppState] = useLocalStorage<TaskAppState>(
     "tasks",
     {
@@ -25,11 +25,11 @@ const TaskApp = (props: TaskAppProp) => {
   };
   return (
     <div>
-    <h1>Task Manager</h1>
-    <TaskForm addTask={addTask} />
-    <h1> <strong>{taskAppState.tasks.length > 0 ? "Pending Tasks" : ""}</strong></h1>
-    <TaskList tasks={taskAppState.tasks} deleteTask={deleteTask} />
-  </div>
+      <h1>Task Manager</h1>
+      <TaskForm addTask={addTask} />
+      <h1> <strong>{taskAppState.tasks.length > 0 ? "Pending Tasks" : ""}</strong></h1>
+      <TaskList tasks={taskAppState.tasks} deleteTask={deleteTask} />
+    </div>
   );
 };
 
