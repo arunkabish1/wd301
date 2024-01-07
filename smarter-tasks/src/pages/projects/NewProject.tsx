382d32ba394524fcfec7ea56ccd8ae20b673fb87
arunkabish1 <720721110060@hicet.ac.in>
2024-01-07 18:47:28 +0530

    l8 resolvng check

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -1,107 +1,111 @@
-import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
-import { API_ENDPOINT } from '../../config/constants';
-import { useForm, SubmitHandler } from 'react-hook-form';
-
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { addProject } from "../../context/projects/actions";
+import { useProjectsDispatch } from "../../context/projects/context";
 type Inputs = {
   name: string;
 };
-
 const NewProject = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
+  const [error, setError] = useState(null);
 
+  const dispatchProjects = useProjectsDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
   const closeModal = () => {
     setIsOpen(false);
   };
-
+  const openModal = () => {
+    setIsOpen(true);
+  };
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const { name } = data;
-    const token = localStorage.getItem('authToken') ?? '';
 
-    try {
-      const response = await fetch(`${API_ENDPOINT}/projects`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
-        body: JSON.stringify({ name }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to create project');
-      }
-
-      const responseData = await response.json();
-      console.log(responseData);
-
+    const response = await addProject(dispatchProjects, { name });
+    if (response.ok) {
       setIsOpen(false);
-    } catch (error) {
-      console.error('Operation failed:', error);
+    } else {
+      setError(response.error as React.SetStateAction<null>);
     }
   };
-
   return (
     <>
       <button
         type="button"
-        id='newProjectBtn'
         onClick={openModal}
+        id="newProjectBtn"
         className="rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
       >
         New Project
       </button>
-
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={closeModal}>
-          <div className="min-h-screen px-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <div className="inline-block align-middle my-8 p-6 max-w-md w-full overflow-hidden text-left transition-all transform bg-blue-100 shadow-xl rounded-2xl">
-              <Dialog.Title as="h3" className="text-lg bg-blue-100 font-medium leading-6">
-                Create New Project !
-              </Dialog.Title>
-
-              <div className="mt-2">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <input
-                    type="text"
-                    id='submitNewProjectBtn'
-                    placeholder="Enter project name..."
-                    autoFocus
-                    {...register('name', { required: true })}
-                    className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
-                      errors.name ? 'border-red-500' : ''
-                    }`}
-                  />
-                  {errors.name && <span className="text-red-500">This field is required</span>}
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
                   >
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    onClick={closeModal}
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                  >
-                    Cancel
-                  </button>
-                </form>
-              </div>
+                    Create new project
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                      {error && <span>{error}</span>}
+                      <input
+                        // name="name"
+                        type="text"
+                        placeholder=" Project name"
+                        autoFocus
+                        {...register("name", { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.name ? "border-red-500" : ""
+                        }`}
+                      />
+                      {errors.name && <span>Field must be filled</span>}
+                      <button
+                        type="submit"
+                        id="submitNewProjectBtn"
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                        Submit
+                      </button>
+                      <button
+                        type="submit"
+                        onClick={closeModal}
+                        className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      >
+                        Cancel
+                      </button>
+                    </form>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
         </Dialog>
@@ -109,5 +113,4 @@ const NewProject = () => {
     </>
   );
 };
-
 export default NewProject;
--- smarter-tasks/src/pages/projects/ProjectListItems.tsx
+++ smarter-tasks/src/pages/projects/ProjectListItems.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function ProjectListItems() {
   let state: any = useProjectsState();
-  const { isError, errorMessage, projects, isLoading } = state;
+  const { projects, isLoading, isError, errorMessage } = state;
+  console.log(projects);
 
   if (projects.length === 0 && isLoading) {
-    return <span>Loading</span>;
+    return <span>Loading...</span>;
   }
 
   if (isError) {
@@ -17,14 +18,14 @@ export default function ProjectListItems() {
     <>
       {projects.map((project: any) => (
         <Link
-        key={project.id}
-        to={`${project.id}`}
-          className="block p-6 bg-blue-100 border border-gray-300 rounded-lg shadow-md hover:bg-blue-200 dark:bg-gray-800 dark:border-blue-700 dark:hover:bg-blue-700 transition duration-300"
+          key={project.id}
+          to={`${project.id}`}
+          className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
           <h5 className="mb-2 text-xl font-medium tracking-tight text-gray-900 dark:text-white">
             {project.name}
           </h5>
-          </Link>
+        </Link>
       ))}
     </>
   );
